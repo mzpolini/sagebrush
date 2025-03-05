29f143fb6e5dd1734b6921aaaac30a9c959a5c4b
mzpolini <matthew.polini@gmail.com>
2025-03-05 06:58:33 -0500

    add dashboard

--- app/profile/[id]/layout.tsx
+++ app/profile/[id]/layout.tsx
@@ -11,6 +11,7 @@ export default async function ProfileLayout({
   return (
     <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <nav className="flex gap-4 py-4">
+        <Link href={`/profile/${id}/dashboard`}>Dashboard</Link>
         <Link href={`/profile/${id}/general`}>General</Link>
         <Link href={`/profile/${id}/applicant`}>Applicant</Link>
         <Link href={`/profile/${id}/investor`}>Investor</Link>
